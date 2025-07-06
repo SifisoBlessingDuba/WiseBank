9946a92312888e57f5cedf5d77a662cdf7ff9568
Fats00 <222830190@mycput.ac.za>
2025-07-06 19:41:42 +0200

    Work resolved

--- src/main/java/za/ac/cput/wisebank/Factory/LoanPaymentFactory.java
+++ src/main/java/za/ac/cput/wisebank/Factory/LoanPaymentFactory.java
@@ -3,11 +3,12 @@ package za.ac.cput.wisebank.Factory;
 import za.ac.cput.wisebank.domain.LoanPayment;
 import za.ac.cput.wisebank.util.Helper;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class LoanPaymentFactory {
 
-    public static LoanPayment createLoanPayment(Integer paymentId, Integer loanId, Date paymentDate, Double amountPaid, String status) {
+    public static LoanPayment createLoanPayment(Integer paymentId, Integer loanId, LocalDateTime paymentDate, Double amountPaid, String status) {
 
         return new LoanPayment.Builder()
                 .setPaymentId(paymentId)
--- src/test/java/za/ac/cput/wisebank/Factory/LoanPaymentFactoryTest.java
+++ src/test/java/za/ac/cput/wisebank/Factory/LoanPaymentFactoryTest.java
@@ -1,11 +1,18 @@
 package za.ac.cput.wisebank.Factory;
 
 import org.junit.jupiter.api.Test;
+import za.ac.cput.wisebank.domain.LoanPayment;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 class LoanPaymentFactoryTest {
 
     @Test
     void createLoanPayment() {
+
+        LoanPayment loanPayment = LoanPaymentFactory.createLoanPayment( 123456, 7891011, LocalDateTime.now(), 50000.00, "paid");
+        assertNotNull(loanPayment);
     }
+
 }
