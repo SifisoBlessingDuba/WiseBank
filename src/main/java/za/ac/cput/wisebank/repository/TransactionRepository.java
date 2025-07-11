6cfa9265b2e676e89f1134e2e83ac65083243b08
BENNET DYANI <b.dyani@plum.systems.com>
2025-07-11 10:57:52 +0200

    I fixed my Entity, Factory and Reposoitory classes

--- src/main/java/za/ac/cput/wisebank/repository/TransactionRepository.java
+++ src/main/java/za/ac/cput/wisebank/repository/TransactionRepository.java
@@ -5,34 +5,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.wisebank.domain.Transaction;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    List<Transaction> findBySenderAccountId(Long senderAccountId);
-    List<Transaction> findByTransactionType(String transactionType);
-    List<Transaction> findByStatus(String status);
-    List<Transaction> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
-
-    Transaction getTransactionById(Long transactionId);
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cput.wisebank.domain.Transaction;
-import java.util.List;
-import java.util.Optional;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
     @Override
     Transaction save(Transaction transaction);
 
     @Override
-    void deleteById(Integer TransactionId);
+    void deleteById(Long TransactionId);
 
     @Override
-    Optional<Transaction> findById(Integer TransactionId);
+    Optional<Transaction> findById(Long TransactionId);
 
     @Override
     List<Transaction> findAll();
