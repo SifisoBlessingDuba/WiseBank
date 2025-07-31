d0b24061bbefeeb3d5721f5d04b5e3825c70b180
BENNET DYANI <b.dyani@plum.systems.com>
2025-07-31 05:51:21 +0200

    Use http://localhost:8080/transaction for testing that its working.

--- src/main/java/za/ac/cput/wisebank/factory/TransactionFactory.java
+++ src/main/java/za/ac/cput/wisebank/factory/TransactionFactory.java
@@ -1,38 +1,30 @@
-package za.ac.cput.wisebank.factory;
+package za.ac.cput.wisebank.Factory;
 
-import za.ac.cput.wisebank.domain.Account;
 import za.ac.cput.wisebank.domain.Transaction;
 import za.ac.cput.wisebank.util.Helper;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public class TransactionFactory {
-    public static Transaction createTransaction(Integer transactionId, Account senderAccount,
-                                                BigDecimal amount, String transactionType,
-                                                LocalDateTime timestamp, String description,
-                                                String status) {
-        if(
-                !Helper.isValidInt(transactionId) ||
-                senderAccount == null ||
-                amount == null || amount.compareTo(BigDecimal.ZERO) <= 0 ||
-                Helper.isNullOrEmpty(transactionType) ||
-                timestamp == null ||
+    public static Transaction createTransaction(Integer senderAccountId, Integer receiverAccountId,
+                                                double amount, String status, String description, LocalDate transactionDate,
+                                                String transactionType) {
+        if(!Helper.isValidInteger(senderAccountId) ||
+                !Helper.isValidInteger(receiverAccountId) ||
+                !Helper.isValidDouble(amount) ||
+                Helper.isNullOrEmpty(status) ||
                 Helper.isNullOrEmpty(description) ||
-                Helper.isNullOrEmpty(status)
-        ) {
+                Helper.isNullOrEmpty(transactionType)){
             return null;
         }
-
         return new Transaction.Builder()
-                .setTransactionId(transactionId)
-                .setSenderAccount(senderAccount)
+                .setSenderAccountId(senderAccountId)
+                .setReceiverAccountId(receiverAccountId)
                 .setAmount(amount)
-                .setTransactionType(transactionType)
-                .setTimestamp(timestamp)
-                .setDescription(description)
                 .setStatus(status)
+                .setDescription(description)
+                .setTransactionDate(transactionDate)
+                .setTransactionType(transactionType)
                 .build();
     }
 }
