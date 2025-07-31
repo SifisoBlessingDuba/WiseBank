2b7e48dd81b20a31a2bcdc7c19cb58f04c0f0313
Laeeqah-Arendse <161837984+Laeeqah-Arendse@users.noreply.github.com>
2025-07-31 20:21:17 +0200

    updated code for factory and beneficiary and message

--- src/main/java/za/ac/cput/wisebank/domain/Beneficiary.java
+++ src/main/java/za/ac/cput/wisebank/domain/Beneficiary.java
@@ -10,9 +10,6 @@ public class Beneficiary {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer beneficiaryId;
 
-
-
-    private int userId;
     private String accountNumber;
     private String name;
     private String bankName;
@@ -28,18 +25,12 @@ public class Beneficiary {
     public Beneficiary(Builder builder) {
         this.beneficiaryId = builder.beneficiaryId;
         this.user = builder.user;
-
-    public Beneficiary(Builder builder) {
-        this.beneficiaryId = builder.beneficiaryId;
-        this.userId = builder.userId;
-
         this.accountNumber = builder.accountNumber;
         this.name = builder.name;
         this.bankName = builder.bankName;
         this.addedAt = builder.addedAt;
     }
 
-    // Getters
     public Integer getBeneficiaryId() {
         return beneficiaryId;
     }
@@ -60,25 +51,6 @@ public class Beneficiary {
         return bankName;
     }
 
-    }
-
-    // Getters
-    public int getBeneficiaryId() {
-        return beneficiaryId;
-    }
-    public int getUserId() {
-        return userId;
-    }
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getBankName() {
-        return bankName;
-    }
-
     public LocalDate getAddedAt() {
         return addedAt;
     }
@@ -87,10 +59,7 @@ public class Beneficiary {
     public String toString() {
         return "Beneficiary{" +
                 "beneficiaryId=" + beneficiaryId +
-
                 ", user=" + (user != null ? user.getId() : null) +
-
-                ", userId=" + userId +
                 ", accountNumber='" + accountNumber + '\'' +
                 ", name='" + name + '\'' +
                 ", bankName='" + bankName + '\'' +
@@ -98,35 +67,21 @@ public class Beneficiary {
                 '}';
     }
 
-
     public static class Builder {
         private Integer beneficiaryId;
         private User user;
-    public static class Builder {
-        private Integer beneficiaryId;
-        private int userId;
         private String accountNumber;
         private String name;
         private String bankName;
         private LocalDate addedAt;
-
-        public Builder() {}
-
 
         public Builder setBeneficiaryId(Integer beneficiaryId) {
             this.beneficiaryId = beneficiaryId;
             return this;
         }
 
-
         public Builder setUser(User user) {
             this.user = user;
-            return this;
-        }
-
-
-        public Builder setUserId(int userId) {
-            this.userId = userId;
             return this;
         }
 
@@ -139,14 +94,17 @@ public class Beneficiary {
             this.name = name;
             return this;
         }
+
         public Builder setBankName(String bankName) {
             this.bankName = bankName;
             return this;
         }
+
         public Builder setAddedAt(LocalDate addedAt) {
             this.addedAt = addedAt;
             return this;
         }
+
         public Beneficiary build() {
             return new Beneficiary(this);
         }
